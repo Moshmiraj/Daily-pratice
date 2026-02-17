a58fa1ebd8ddc450ec6949bd2241e9880b3fd3c6
Moshmi Raj <moushmiraj72@gmail.com>
2026-02-17 22:00:01 +0530

    Add files via upload

---
+++ Maximum_array.java
@@ -0,0 +1,14 @@
+ public class Maximum_array {
+    public static void main(String[] args) {
+        int[] array = { 3 , 5 , 7 ,2 };
+        int max = array[0];
+        for( int i = 1 ; i < array.length ; i++){
+            if (array[i] > max){
+                max = array[i];
+            }
+        }
+        System.out.println("Maximum: " + max);
+    }
+}
+ 
+ 
